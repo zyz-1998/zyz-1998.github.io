ddb114bf2afdd20828d70bd8376fee638692d62d
zyz98nianran@gmail.com <39764291+jokerzyz@users.noreply.github.com>
2019-08-19 17:10:47 +0800

    Site updated: 2019-08-19 17:10:40

--- js/next-boot.js
+++ js/next-boot.js
@@ -1,21 +1,29 @@
 /* global NexT, CONFIG */
 
-$(document).on('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', () => {
 
-  CONFIG.back2top.enable && NexT.utils.registerBackToTop();
+  NexT.utils.registerScrollPercent();
   NexT.utils.registerCanIUseTag();
 
   // Mobile top menu bar.
-  $('.site-nav-toggle button').on('click', function() {
+  document.querySelector('.site-nav-toggle button').addEventListener('click', () => {
     var $siteNav = $('.site-nav');
     var ON_CLASS_NAME = 'site-nav-on';
     var isSiteNavOn = $siteNav.hasClass(ON_CLASS_NAME);
     var animateAction = isSiteNavOn ? 'slideUp' : 'slideDown';
     var animateCallback = isSiteNavOn ? 'removeClass' : 'addClass';
 
-    $siteNav.stop()[animateAction]('fast', function() {
+    $siteNav.stop()[animateAction]('fast', () => {
       $siteNav[animateCallback](ON_CLASS_NAME);
     });
+  });
+
+  window.addEventListener('hashchange', () => {
+    var tHash = location.hash;
+    if (tHash !== '' && !tHash.match(/%\S{2}/)) {
+      var target = document.querySelector(`.tabs ul.nav-tabs li a[href="${tHash}"]`);
+      target && target.click();
+    }
   });
 
   // Define Motion Sequence & Bootstrap Motion.
@@ -31,17 +39,7 @@ $(document).on('DOMContentLoaded', function() {
   }
 });
 
-$(document).on('DOMContentLoaded pjax:success', function() {
-
-  if (CONFIG.save_scroll) {
-    // Read position from localStorage
-    var value = localStorage.getItem('scroll' + location.pathname);
-    $('html, body').animate({ scrollTop: value || 0 });
-    // Write position in localStorage
-    NexT.utils.saveScrollTimer = setInterval(function() {
-      localStorage.setItem('scroll' + location.pathname, $(window).scrollTop());
-    }, 1000);
-  }
+$(document).on('DOMContentLoaded pjax:success', () => {
 
   /**
    * Register JS handlers by condition option.
@@ -62,37 +60,13 @@ $(document).on('DOMContentLoaded pjax:success', function() {
    * Init Sidebar & TOC inner dimensions on all pages and for all schemes.
    * Need for Sidebar/TOC inner scrolling if content taller then viewport.
    */
-  function updateSidebarHeight(height) {
-    height = height || 'auto';
-    $('.site-overview, .post-toc').css('max-height', height);
-  }
-
   function initSidebarDimension() {
-    var updateSidebarHeightTimer;
-
-    $(window).on('resize', function() {
-      updateSidebarHeightTimer && clearTimeout(updateSidebarHeightTimer);
-
-      updateSidebarHeightTimer = setTimeout(function() {
-        var sidebarWrapperHeight = document.body.clientHeight - NexT.utils.getSidebarSchemePadding();
-
-        updateSidebarHeight(sidebarWrapperHeight);
-      }, 0);
-    });
-
-    // Initialize Sidebar & TOC Width.
-    var scrollbarWidth = NexT.utils.getScrollbarWidth();
-    if ($('.site-overview-wrap').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
-      $('.site-overview').css('width', `calc(100% + ${scrollbarWidth}px)`);
-    }
-    if ($('.post-toc-wrap').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
-      $('.post-toc').css('width', `calc(100% + ${scrollbarWidth}px)`);
-    }
-
     // Initialize Sidebar & TOC Height.
-    updateSidebarHeight(document.body.clientHeight - NexT.utils.getSidebarSchemePadding());
+    var sidebarWrapperHeight = document.body.clientHeight - NexT.utils.getSidebarSchemePadding();
+    $('.site-overview, .post-toc').css('max-height', sidebarWrapperHeight);
   }
   initSidebarDimension();
+  window.addEventListener('resize', initSidebarDimension);
 
   function wrapTable() {
     $('table').not('.gist table').wrap('<div class="table-container"></div>');
